c6d50957c62b5b3292ed0ae8dd9fc800da05cc02
Bobby Moynihan <bobby@gigfirm.com>
2022-02-13 13:39:47 -0600

    hard code global seo instead of fetching from strapi

--- utils/seo.js
+++ utils/seo.js
@@ -1,5 +1,11 @@
 import { getStrapiMedia } from './medias'
 
+export const globalSeo = {
+  siteName: 'SRPCO',
+  description: 'Custom Gasket Fabrication & Die-cutting Solutions Since 1951',
+  image: '/favicon.png'
+}
+
 export function getMetaTags (seo) {
   const tags = []
 
@@ -57,4 +63,33 @@ export function getMetaTags (seo) {
   tags.push({ name: 'twitter:card', content: 'summary_large_image' })
 
   return tags
+}
+
+
+export const seoHead = (page) => {
+    const { defaultSeo = {}, siteName = 'SRPCO' } = [null, undefined].includes(globalSeo) ? {} :globalSeo
+    let { title = page?.title || null, image = page?.media || null, description = page?.description || null } = !!page && !!page.seo ? page.seo : {}
+    if (!!!image && !!page?.image) { image = page?.image; }
+    if (!!!image && !!defaultSeo.image) { image = defaultSeo.image; }
+    if (!!!title && !!page?.title) { title = page?.title; }
+    if (!!!title && !!defaultSeo?.title) { title = defaultSeo?.title; }
+    if (!!!description && !!page?.description) { description = page?.description; }
+    if (!!!description && !!defaultSeo.description) { description = defaultSeo.description; }
+    const seo = { title, image, description };
+
+    const fullSeo = {
+      ...defaultSeo,
+      ...Object.keys(seo).reduce((acc, key) => {
+        if (seo[key] === null || typeof seo[key] === 'undefined') {
+          return acc
+        }
+        return { ...acc, [key]: seo[key] }
+      }, {})
+    }
+    const meta = getMetaTags(fullSeo)
+    return {
+      titleTemplate: `%s | ${siteName}`,
+      title: fullSeo.title,
+      meta
+    }
 }
