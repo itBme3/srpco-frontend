7868fd48f64be1c44f1d5efcd4d308bfde1a9a06
Bobby Moynihan <bobby@gigfirm.com>
2022-02-27 15:06:54 -0600

    site search box

--- components/SiteHeaderSearchBox.vue
+++ components/SiteHeaderSearchBox.vue
@@ -1,52 +1,109 @@
 <template>
   <div
-    class="flex flex-wrap space-y-2 sm:space-x-2"
+    class="flex flex-wrap space-y-2 sm:space-x-2 gap-2"
     :class="{
         'expanded opacity-100 translate-y-0 top-12 mt-[2px] animate-fade-in-down': isExpanded,
         'opacity-0 -translate-y-full -top-40': !isExpanded
       }"
   >
-    <Blocks
+    <!-- <Blocks
       v-if="!!siteSearch && Array.isArray(siteSearch.blocks) && siteSearch.blocks.length > 0"
       :blocks="siteSearch.blocks"
-    />
-    <!-- SECTIONS -->
-    <!-- <div
-      class="site-search-section"
-      v-for="(section, i) in sections"
-      :key="'site-search-section-' + i"
-      :class="{ ['' + ![undefined, null].includes(section.classes) ? section.classes.section : null + '']: ![undefined, null].includes(section.classes) && typeof section.classes.section === 'string' && section.classes.section.length > 0 }"
-    >
+    /> -->
 
-      <h2
-        class="section-title"
-        :class="{ ['' + ![undefined, null].includes(section.classes) ? section.classes.title : null + '']: ![undefined, null].includes(section.classes) && typeof section.classes.title === 'string' && section.classes.title.length > 0 }"
-      >{{section.title}}</h2>
+    <div class="grid grid-cols-12">
 
-      <div
-        class="section-buttons flex items-center"
-        :class="{ ['' + ![undefined, null].includes(section.classes) ? section.classes.buttons : null + '']: ![undefined, null].includes(section.classes) && typeof section.classes.buttons === 'string' && section.classes.buttons.length > 0 }"
-        v-if="Array.isArray(section.buttons) && section.buttons.length > 0"
-      >
-        <Link
-          v-for="(btn, ii) in section.buttons"
-          :key="'site-search-section-' + i + '-button-' + ii"
-          :link="btn.link"
-        >
-        <gButton
-          class="whitespace-nowrap"
-          :class="{ ['' + btn.classes + '']: typeof btn.classes === 'string' && btn.classes.length > 0 }"
-        >
-          {{ btn.text }}
-        </gButton>
-        </Link>
+      <div class="col-span-12 sm:col-span-7 sm:pr-6 rounded-md">
+        <div class="custom-gaskets section col-span-6 border-gray-900 rounded-md border-2 border-opacity-25 p-6 flex flex-wrap mt-4">
+          <h3 class="section-title w-full mb-4">Custom Gaskets</h3>
+          <gButton
+            @click="$router.push('/applications')"
+            class="bg-opacity-50 w-[calc(33.33%-.25rem)] mr-1"
+          >
+            By Application
+          </gButton>
+          <gButton
+            @click="$router.push('/materials')"
+            class="bg-opacity-50 w-[calc(33.33%-.25rem)] mr-1"
+          >
+            By Material
+          </gButton>
+          <gButton
+            @click="$router.push('/gaskets')"
+            class="bg-opacity-50 w-[calc(33.33%-.25rem)]"
+          >
+            All Gaskets
+          </gButton>
+        </div>
+        <div class="resources section col-span-6 flex flex-wrap px-0 mt-4">
+          <template v-for="(link, i) in ['Solutions', 'Services', 'Datasheets', 'Resources']">
+            <gButton
+              :class="{
+                'border border-gray-700 hover:bg-gray-700 w-[calc(50%-.25rem)] mb-2': true,
+                'mr-2': [0, 2].includes(i),
+              }"
+              :key="link"
+              @click="$router.push('/' + link.toLowerCase())"
+            >
+              {{ link }}
+            </gButton>
+          </template>
+        </div>
+
       </div>
 
-    </div> -->
+      <div class="col-span-12 sm:col-span-5">
+
+        <div class="contact section col-span-8 flex space-x-6 border-l-0 border-r-0 border-gray-700 py-6">
+          <nuxt-link
+            to="/locations"
+            class="flex items-center justify-start whitespace-nowrap text-xl"
+          >
+            <Icon
+              icon-name="location"
+              class="mr-2 bg-transparent text-green-500"
+            /> <span>Locations</span>
+          </nuxt-link>
+          <a
+            href="tel:+18003336322"
+            class="flex items-center justify-start whitespace-nowrap text-xl"
+          >
+            <Icon
+              icon-name="phone"
+              class="mr-2 text-red-400"
+            /> <span>Call Us</span>
+          </a>
+        </div>
+
+        <div class="p-6 bg-gray-700 bg-opacity-50 rounded-md">
+          <h3 class="text-gray-400">SRP is a Preferred Converter</h3>
+          <gButton
+            @click="$router.push('/suppliers')"
+            class="bg-gray-800 hover:bg-gray-700 text-gray-400 rounded-full mt-5 text-sm shadow-lg w-auto mr-auto"
+          >
+            why that matters
+          </gButton>
+        </div>
+      </div>
+
+      <div class="col-span-12 flex items-center content-start py-8 sm:py-12 mt-6 sm:pr-16 px-4 border border-gray-900 border-l-0 border-r-0 border-b-0">
+        <div class="w-3/4 text-center">
+          <h1 class="text-center">Free Consultation</h1>
+          <h5 class="text-blue-500 text-center">TYPICAL RESPONSE WITHIN 48 BUSINESS HOURS</h5>
+        </div>
+        <gButton
+          @click="$router.push('/contact')"
+          class="bg-blue-500 text-blue-900 mt-5 shadow-lg mr-auto w-1/4 text-xl uppercase font-bold"
+        >
+          Schedule<br> Consultation
+        </gButton>
+      </div>
+
+    </div>
 
     <!-- CLOSE -->
     <gButton
-      classes="rounded-md px-3 py-2 hover:bg-gray-200 hover:bg-opacity-5 absolute right-1 -top-1 w-12"
+      classes="rounded-md px-3 py-2 bg-transparent hover:bg-gray-200 hover:bg-opacity-5 absolute right-1 -top-1 w-12"
       @click="isExpanded = false; $emit('close')"
     >
       <i class="gicon-close text-red-400" />
@@ -96,6 +153,17 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.section {
+  @apply p-2;
+}
+.section-title {
+  @apply text-gray-600;
+}
+
+button {
+  @apply bg-gray-700 text-gray-300 mb-1;
+}
+
 .nuxt-link-exact-active {
   @apply cursor-default;
   button {
