47ea60cb639ac07176871b7e8df4c12bc9754517
Bobby Moynihan <bobby@gigfirm.com>
2022-05-15 14:08:44 -0500

    contact form updates

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="page contact">
+  <div class="page contact mx-auto max-w-lg">
     <h1 class="text-center pb-8 pt-14">Contact Us</h1>
-    <Form
-      class="mx-auto max-w-lg"
-      :schema="formSchema"
-      :model="model"
-      success-redirec="/gaskets"
-      @submit="submit"
-    />
+    <template
+      v-if="['ready', 'error'].includes(formState)"
+    >
+      <template
+        v-if="formState === 'error'"
+      >
+      <h3 class="text-center mb-2 text-red-500">Error:</h3>
+      <p v-if="errorMessage" class="text-center" >{{errorMessage}}</p>
+      </template>
+      <Form
+        ref="form"
+        class="mx-auto"
+        :schema="formSchema"
+        :model="model"
+        @submit="submit"
+      />
+    </template>
+    <template v-else-if="formState === 'sending'">
+      <h3 class="text-center">Sending...</h3>
+      <Loading class="mx-auto" />
+    </template>
+    <template v-else-if="formState === 'success'">
+      <h3 class="text-center mb-2 text-green-400">Message sent</h3>
+      <p class="text-center">An SRP team member will reaching out shortly.</p>
+      <gButton 
+        class="text-sm bg-gray-800 text-gray-200 mt-8 mx-auto w-auto"
+        @click="resetForm">RESET</gButton>
+    </template>
   </div>
 </template>
 
@@ -74,16 +95,23 @@ export default Vue.extend({
         email: '',
         hearAboutUs: '',
         file: '',
-        message: ''
-      }
+        message: '',
+      },
+      formState: 'ready',
+      errorMessage: null
     }
   },
   methods: {
-    successfulSubmit (data) {
-      console.log(data)
-    },
-    submitCallback (data) {
-      return data;
+    resetForm() {
+      this.model = {
+        name: '',
+        email: '',
+        hearAboutUs: '',
+        file: '',
+        message: '',
+      }
+      this.formState = 'ready'
+      this.errorMessage = null
     },
     toBase64 (file) {
       return new Promise((resolve, reject) => {
@@ -93,13 +121,12 @@ export default Vue.extend({
         reader.onerror = error => reject(error);
       });
     },
-    async submit (e) {
-      e.preventDefault();
+    async submit (formModel) {
       this.formState = 'sending'
       try {
-        const model = JSON.parse(JSON.stringify(this.formModel));
-        if (Object.keys(model).includes('file')) {
-          const file = this.$refs.form.querySelector('input[type="file"]').files[0];
+        const model = JSON.parse(JSON.stringify(formModel));
+        const file = this.$refs.form.$el.querySelector('input[type="file"]').files[0];
+        if (file) {
           const maxSize = 1000000 * 2; // 2mb
           if (file.size > maxSize) {
             this.formState = 'error';
@@ -110,16 +137,11 @@ export default Vue.extend({
           model.file = fileBase64;
         }
         let res = null;
-        if ([null, undefined].includes(this.submitCallback)) {
-          res = await this.$axios.post(`${process.env.apiUrl}/api/email-contact`, model)
-          if (res.error?.length) {
-            this.formState = 'error';
-            this.errorMessage = `<h2>Error</h2><p>${res.error}</p>`
-            return this.$emit('error', res.error)
-          }
-        } else {
-          const submitted = this.submitCallback(model);
-          res = submitted?.then ? await submitted : submitted;
+        res = await this.$axios.post(`${process.env.apiUrl}/api/email-contact`, model)
+        if (res.error?.length) {
+          this.formState = 'error';
+          this.errorMessage = `<h2>Error</h2><p>${res.error}</p>`
+          return this.$emit('error', res.error)
         }
         this.formState = 'success';
         this.$emit('success', model)
